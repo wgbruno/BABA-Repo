a3bb53bd057a30b3d162b16e044848df7c7b6deb
Hunter Malone <hmalone@loyola.edu>
2022-10-22 17:44:29 -0400

    Added beginnings for player and stats classes.

---
+++ BABA_App/Objects/Player.js
@@ -0,0 +1,13 @@
+import {Stats} from './Stats.js';
+export class Player {
+    constructor(firstName, lastName, number, age, height, stats){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.age = age;
+        this.height = height;
+        this.stats = stats;
+    }
+}
+
+ 
---
+++ BABA_App/Objects/Stats.js
@@ -0,0 +1,11 @@
+export class Stats{
+    constructor(points, rebounds, assists, blocks, steals, fouls, turnovers){
+        this.points = points;
+        this.rebounds = rebounds;
+        this.assists = assists;
+        this.blocks = blocks;
+        this.steals = steals;
+        this.fouls = fouls;
+        this.turnovers = turnovers;
+    }
+}
