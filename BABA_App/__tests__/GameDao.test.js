08bfb2efe91e6263c5f79fedaf4cda210483859d
KEENAN KIRK <kakirk@loyola.edu>
2022-11-11 19:42:38 -0500

    Game View #4 Testing framework but gives Realm error before running

---
+++ BABA_App/__tests__/GameDao.test.js
@@ -0,0 +1,19 @@
+import {expect, jest, test} from '@jest/globals';
+import { Game } from '../Objects/GameCont';
+import { insertDBGame, getAllDBGames, DBisEmpty } from '../DAOs/GameDao.js'
+
+test("Empty Database Detected", function (){
+    expect(DBisEmpty).toBeTruthy();
+})
+
+test("Create a Game in Database", function (){
+    let game = new Game("Ballers", "Ringers", "11/20", "9", 10, 4);
+    expect(DBisEmpty).toBeFalsy();
+})
+
+/*
+test("Get all Games from DB", function (){
+    //need to figure this one out
+    expect(DBisEmpty).toBeTruthy();
+})*/
+
