e339e12a75c0338895f542a491383522e4798683
Hunter Malone <hmalone@loyola.edu>
2022-12-01 13:53:42 -0500

    Removed old player code and fixed schema

--- BABA_App/App.js
+++ BABA_App/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from "react";
 import { Button, Text, TextInput, TouchableOpacity, View, StyleSheet, SafeAreaView } from 'react-native';
-import AddPlayer from "./View/AddPlayer.js";
 import AddTeam from "./View/AddTeam.js";
 import Home from "./View/Home.js";
 import SignInScreen from "./View/Account/SignInScreen/SignInScreen.js";
@@ -9,14 +8,13 @@ import ConfirmEmailScreen from "./View/Account/ConfirmEmailScreen/ConfirmEmailSc
 import ForgotPasswordScreen from "./View/Account/ForgotPasswordScreen/ForgotPasswordScreen.js";
 import NewPasswordScreen from "./View/Account/NewPasswordScreen/NewPasswordScreen.js";
 import DeleteAccountScreen from "./View/Account/DeleteAccount/DeleteAccount.js";
-import ViewAllPlayers from "./View/ViewAllPlayers.js";
-import EditPlayer from "./View/EditPlayer.js";
 import ViewAllTeams from "./View/ViewAllTeams.js";
 import EditTeam from "./View/EditTeam.js";
 import Calendar from './View/pages/Calendar'
 import CreateGame from "./View/AddGame.js";
 import GameScoreScreen from "./View/LiveScore/GameScoreScreen.js";
 import ManagerHomeScreen from "./View/Manager/ManagerHomeScreen.js";
+import DisplayTeamScren from './View'
 import FormStyle from "./Style/Form.style";
 import {createAppContainer} from 'react-navigation'; 
 import {createStackNavigator} from 'react-navigation-stack';
@@ -30,9 +28,6 @@ const AppNavigator = createStackNavigator(
       ForgotPassword: ForgotPasswordScreen,
       NewPassword: NewPasswordScreen,
       DeleteAccount: DeleteAccountScreen,
-      PlayerReg: AddPlayer,
-      ViewPlayers: ViewAllPlayers, 
-      UpdatePlayers: EditPlayer,
       TeamReg: AddTeam,
       ViewTeams: ViewAllTeams,
       UpdateTeams: EditTeam,
